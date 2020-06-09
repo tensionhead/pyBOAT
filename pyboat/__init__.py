3dfe05dbbcadbafdd27fb1ec40b794fb62022016
gregor <gregor.moenke@embl.de>
2020-06-09 15:55:10 +0200

    the icon

--- pyboat/__init__.py
+++ pyboat/__init__.py
@@ -39,9 +39,10 @@ def main(argv=None):
     app = QApplication(sys.argv) 
 
     # add an application icon
-    abs_path = os.path.dirname(os.path.realpath(__file__)) 
+    abs_path = os.path.dirname(os.path.realpath(__file__))
+    icon_path = os.path.join(abs_path, 'logo_circ128x128.png')
     icon = QIcon()
-    icon.addFile(abs_path + '/logo_circ128x128.png', QSize(128,128))    
+    icon.addFile(icon_path, QSize(128,128))    
     app.setWindowIcon(icon)        
 
     # -- parse command line arguments ---

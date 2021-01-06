795353715e5ae396134af27b2fd54babf672d4b5
tensionhead <gregor.moenke@embl.de>
2021-01-06 18:31:21 +0100

    ensemble scripting demo

---
+++ ensemble_demo.py
@@ -0,0 +1,73 @@
+'''
+A short demonstration of the provided
+ensemble measures
+'''
+import numpy as np
+import pandas as pd
+from pyboat import ensemble_measures as em
+from pyboat import WAnalyzer, ssg
+from pyboat import plotting as pl 
+
+# set up analyzing instance
+periods = np.linspace(5, 60, 100)
+dt = 1
+wAn = WAnalyzer(periods, dt)
+
+# create a bunch of chirp signals
+# with different final period
+Nsignals = 50 # times 2
+Tstart = 30 # initial period
+Tend = 50   # period at the end
+Nt = 500 # number of samples per signal
+signals = [
+    ssg.create_noisy_chirp(T1 = Tstart, T2 = T, Nt = Nt, eps = 1)
+    for T in np.linspace(Tstart, Tend, Nsignals)
+]
+
+# add the same number of pure noise signals
+noisy_ones = [
+    ssg.ar1_sim(alpha = 0.5, N = Nt)
+    for i in range(Nsignals)
+]
+
+# signals ids are just column numbers here
+signals = pd.DataFrame(signals + noisy_ones).T
+
+# get the the individual ridge readouts
+ridge_results = {}
+
+# store the individual time averaged Wavelet spectra
+df_fouriers = pd.DataFrame(columns = np.arange(len(signals)), index = wAn.periods)
+
+for ID in signals:
+
+    wAn.compute_spectrum(signals[ID], do_plot = False)
+    rd = wAn.get_maxRidge(smoothing_wsize = 11)
+    ridge_results[ID] = rd
+
+    df_fouriers[ID] = wAn.get_averaged_spectrum()
+
+# the time-averaged power distribution,
+# index holds the signal ids if given, otherwise just a column index
+powers_series = em.average_power_distribution(
+    ridge_results.values(),
+    signal_ids = ridge_results.keys())
+
+# is bi-modal!
+pl.power_distribution(powers_series)
+
+# filter out the pure noise signals with a power threshold
+high_power_ids = powers_series[powers_series > 10].index
+high_power_ridge_results = [ridge_results[ID] for ID in high_power_ids]
+
+res = em.get_ensemble_dynamics(high_power_ridge_results)
+
+pl.ensemble_dynamics(*res)
+
+# the Fourier power distribution with pure noise signals
+ax = pl.Fourier_distribution(df_fouriers)
+ax.set_title('Whole ensemble', pad = -40)
+
+# the Fourier power distribution without pure noise signals
+ax = pl.Fourier_distribution(df_fouriers[high_power_ids])
+ax.set_title('Noise filtered out', pad = -40)
